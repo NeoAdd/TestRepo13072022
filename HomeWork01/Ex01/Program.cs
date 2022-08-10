c6c60747b73098f9999d412bd4b586a27f154916
Alex <Lesnik290199@gmail.com>
2022-08-10 19:26:27 +0500

    решил первую задачу семинара 1

--- HomeWork01/Ex01/Program.cs
+++ HomeWork01/Ex01/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Hello, World!");
+int a = 5;
+int b = 7;
+if (a > b)
+{
+     //Console.WriteLine("число a max, число b min");
+     Console.WriteLine($" {a} - max,  {b} - min");                   
+}
+else if (a < b )
+{
+   //Console.WriteLine("число a min, число b max"); 
+   Console.WriteLine($" {a} - min,  {b} - max");                  
+}
