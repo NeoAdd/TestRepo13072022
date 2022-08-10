c105cc678399afad1a912e664fafce21c4ab09d9
Alex <Lesnik290199@gmail.com>
2022-08-10 22:36:42 +0500

    Создал блок схему к задаче 2 семинара  1

--- HomeWork01/Ex02/Program.cs
+++ HomeWork01/Ex02/Program.cs
@@ -1,3 +1,29 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Hello, World!");
+int a = 30;
+int b = 31;
+int c = 29;
+int summ = 36; //a + b + c; 
+
+while (summ > a || summ > b || summ > c )
+{
+summ--;
+}
+
+Console.WriteLine( summ );
+
+//for (int summ = a + b + c ; summ > c || summ > b || summ > b ; summ-- ) 
+//{ 
+      //if (summ == a || summ == b || summ == c);
+      //{
+      //        Console.WriteLine( summ );        
+      //}              
+//Console.WriteLine( summ );
+//}
+//else 
+//{
+//int summ = summ - 1 ;
+//}
+   //Console.WriteLine($" {summ} - min,  {summ} - max");                  
+
+//return "" ;
