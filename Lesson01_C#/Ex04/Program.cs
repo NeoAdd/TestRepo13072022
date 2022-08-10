cf13fbbf6e2179332d3a80de4f4de8522345744c
Alex <Lesnik290199@gmail.com>
2022-08-10 18:52:12 +0500

    начал делать ДЗ семинар 1

--- Lesson01_C#/Ex04/Program.cs
+++ Lesson01_C#/Ex04/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача из семинара
 //Console.WriteLine("Программа для нахождения целых чисел в промежутке от положительного и отрицательного");
 Console.Write("Введите число: ");
-int value = Convert.ToInt32(Console.Readline());
-//int value = 4;
+//int value = Convert.ToInt32(Console.Readline());
+int value = 4;
 int count = value * (-1);
 
 while(count <= value) // знак ; не ставим
