1fddc70a3940e976bb01cd8d6b4cd7e833942608
Alex <Lesnik290199@gmail.com>
2022-08-11 15:42:50 +0500

    решаю задачу 2 ДЗ Семинара 2

---
+++ HomeWork02/Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+int valuet1 = 64; 
+//int valuet2 = 78;
+//int valuet3 = 32679;
+
+
+int result1 = valuet1 / 10 / 10 % 10;  
+//int result2 = valuet2 / 10 / 10 % 10;
+//int result3 = valuet3 / 10 / 10 % 10;
+
+ if (result1 < 1) 
+ {
+                Console.WriteLine($" {valuet1} ->>получаем число {result1}");      
+ }
+else if (result1 < 0 ) 
+{
+Console.WriteLine($" {valuet1} ->> третьей цифры нет");
+}
+
+//Console.WriteLine($" {valuet2} ->>получаем число {result2}");
+//Console.WriteLine($" {valuet3} ->>получаем число {result3}");
