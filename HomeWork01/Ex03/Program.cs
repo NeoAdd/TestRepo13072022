c0899c4fc1c714e238d002514dac05019fdde29d
Alex <Lesnik290199@gmail.com>
2022-08-11 10:15:38 +0500

    проверка задачи 3 семинара 1

--- HomeWork01/Ex03/Program.cs
+++ HomeWork01/Ex03/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+int a;
+Console.WriteLine("Введите число: ");
+a= Console.Read ();
+if (a % 2 ==1) 
+{
+                      Console.WriteLine("число N нечетное");
+}
