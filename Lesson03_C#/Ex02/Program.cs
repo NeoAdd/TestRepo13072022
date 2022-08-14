9e9ab91b08608c7f3a5107fcdf280966b51ecbce
Alex <Lesnik290199@gmail.com>
2022-08-14 22:59:44 +0500

    цикл for

---
+++ Lesson03_C#/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿// ЦИКЛ for
+
+string Method4(int count,string text)   //count это количество раз
+{
+                      string result = string.Empty;
+                      for (int i = 0; i < count; i++)
+                      {
+                         result = result + text;
+                      }
+                     return result;
+}
+
+string res = Method4(10, "0_0 ");
+Console.WriteLine (res);
