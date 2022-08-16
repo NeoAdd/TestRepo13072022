b800874f16d490865b055b4b60c25d984fcb5c31
Alex <Lesnik290199@gmail.com>
2022-08-16 14:48:05 +0500

    MyLibrarium отредактировал

--- Methods/MyLibrarium/MyLibrarium.cs
+++ Methods/MyLibrarium/MyLibrarium.cs
@@ -53,3 +53,37 @@ int number, result = 0;
  
     if (neg) result *= -1;
     Console.WriteLine(result);
+
+
+         //Проверка числа на полидром
+
+    int number, result = 0;
+    bool neg = false;
+int numb;
+    Console.Write("Введите число: ");
+    //4number = Int32.Parse(Console.ReadLine());
+    number = Convert.ToInt32(Console.ReadLine());
+    numb = number;
+ 
+    if (number < 0) 
+    {
+        neg = true;
+        number *= -1;
+    }
+ 
+    while(number > 0)
+    {
+        result *= 10;
+        result += number % 10;
+        number /= 10;
+    }
+ 
+    if (neg) result *= -1;
+
+    //Console.WriteLine(result);
+    
+    if(numb != result)
+
+    Console.WriteLine("нет, не полидром");
+    else
+    Console.WriteLine("да, полидром");
