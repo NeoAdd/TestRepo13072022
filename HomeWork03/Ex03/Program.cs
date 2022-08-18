21c8eded0d6bd203cba0e7157b83b4b9be5f81a4
Alex <Lesnik290199@gmail.com>
2022-08-18 17:46:16 +0500

    решил задачу 3 дз семинара 3

---
+++ HomeWork03/Ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите метод, который принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int i = 1;
+int n = 0;
+int result = 0;
+n = Convert.ToInt32(Console.ReadLine());
+
+while (i < n+1)
+{
+      result = i * i * i;
+      //Math.Pow(i,i);    // не получается
+      i ++;
+      Console.WriteLine(result);
+}
