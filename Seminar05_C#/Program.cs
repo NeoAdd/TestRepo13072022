d4d159d2ea13e7184d96f97c85e0eac4a2be6829
Alex <Lesnik290199@gmail.com>
2022-08-19 09:58:08 +0500

    работа семинара 5

---
+++ Seminar05_C#/Program.cs
@@ -0,0 +1,11 @@
+﻿// Семинар 5  дописать с лекции
+//Console.WriteLine("Hello, World!");
+int [] Create(int n)
+{
+    return new int[n];
+}
+
+string int ArrayToString(int[] col)
+{
+    return $""
+}
