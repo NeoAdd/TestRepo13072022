61e0cfc336735da3cde84abb5e7ed61aa648b162
Alex <Lesnik290199@gmail.com>
2022-08-21 10:08:14 +0500

    работа семинара 5 разбор задачи массива

--- Seminar05_C#/Program.cs
+++ Seminar05_C#/Program.cs
@@ -1,11 +1,17 @@
-﻿// Семинар 5  дописать с лекции
-//Console.WriteLine("Hello, World!");
-int [] Create(int n)
-{
-    return new int[n];
-}
+﻿//Вводим число и разбиваем на массив
 
-string int ArrayToString(int[] col)
+//using System.Text;
+//int value = Convert.ToInt32(Console.ReadLine());
+int value = 1234567;
+value = Math.Abs(value);
+
+int size = value.ToString().Length;
+int[] numbers = new int[size];
+
+while (value != 0) ;
 {
-    return $""
+                      numbers[size - 1] = value % 10;
+                      value /= 10;
+                      size--;
 }
+Console.WriteLine($"value: {string.Join(' ', numbers)}");
