21f2682a1d3dd4d6b4bf383f5e7c260a2ec94f75
Alex <Lesnik290199@gmail.com>
2022-08-24 13:53:04 +0500

    разобрал все  лекции 4

---
+++ Lesson04_C#/Ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿// РЕКУРСИЯ
+
+double Factorial (int n)  // Можно тип данных поменять Double  или int
+{
+    //если дошли 1 то вернем 1
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+
+    //если не 1 тогда
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    //Console.WriteLine(Factorial(i));  // i для запуска цикла
+    Console.WriteLine($"{i}! = {Factorial(i)}"); // Для демонстрации  какой факт считает
+}
+//System.Console.WriteLine(Factorial(3)); //1*2*3 = 6 // можно сдесь изменить 3 на 5
+
+
+
+
