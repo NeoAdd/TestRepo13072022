b7d8cf6021d0d113747dc07c8fe1651d2cb137fb
Alex <Lesnik290199@gmail.com>
2022-08-25 16:58:19 +0500

    Решил задачу 2 ДЗ семинара 5

---
+++ HomeWork05/Ex02/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, 
+//заполненный случайными числами. Найдите сумму элементов, 
+//стоящих на нечётных позициях.
+
+/* [3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+var rnd = new Random();
+var set = new HashSet<int>();
+var arr = new int[6];
+//int count = 0;
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+                      int rand;
+                      do
+                      {
+                                            rand = rnd.Next(-10, 10);
+                      } while (set.Contains(rand));
+                      set.Add(rand);
+                      arr[i] = rand;
+                      
+                      //if (i % 2 == 0)
+                      //count++;
+                      //sum = sum + i;
+                        sum = arr[1] + arr[3] + arr[5];
+                      //Console.WriteLine(arr[i]);
+}
+
+foreach (var i in arr)
+{
+
+                      Console.Write("{0}  ", i);
+}
+  
+//Console.WriteLine($"Всего четных чисел в массиве ->: {(count)}");
+Console.WriteLine($"Сумма элементов стоящих на нечётных позициях ->: {(sum)}");
+
+
+
+
+
+
+
+
