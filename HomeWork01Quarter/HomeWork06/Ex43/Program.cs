ccb8e789345ac29a994c1e2dcbd0748968438ed4
Alex <Lesnik290199@gmail.com>
2022-08-29 19:54:34 +0500

    решил 2 задачу  дз семинара 6

--- HomeWork01Quarter/HomeWork06/Ex43/Program.cs
+++ HomeWork01Quarter/HomeWork06/Ex43/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт 
+//точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)//
+
+
+
+
+Console.WriteLine("Находим точку пересечения двух прямых");
+Console.WriteLine("Введите значения b1: ");
+
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double result = 0;
+double result1 = 0;
+
+result = (b2 - b1) / (k1 - k2);
+result1 = k1 * result + b1;
+
+Console.WriteLine($" точка пересечения({result} ; {result1})");
+
+/* if (result1 == result)
+{
+    Console.WriteLine("прямые не пересекаются: ");
+}
+ */
---
+++ Lesson01Quarter/Lesson06_C#/Ex01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Лекция 6
+
+using System.Linq; // дополнительный модуль
+
+string text = "(1,2) (2,3) (4,5) (5,6)";
+                //.Replace("(", "")
+                //.Replace(")", "")
+                //;
+//Console.WriteLine(text);
+
+
+
+var data = text.Split(" ") // метод Split для разбивки сивола разделителя
+                .Select(item => item.Split(','))
+                .ToArray();
+
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data [i]);
+}
+
+
+
+
+
+
+
