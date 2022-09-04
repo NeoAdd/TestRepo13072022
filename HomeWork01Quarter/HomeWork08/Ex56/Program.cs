32493a34f475cb2b50bc8da28c7e2dc734b419e8
Alex <Lesnik290199@gmail.com>
2022-09-05 01:03:39 +0500

    решил задачу 56 семинара 8

---
+++ HomeWork01Quarter/HomeWork08/Ex56/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить 
+//строку с наименьшей суммой элементов.
+
+/* Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7 
+*/
+
+int[,] array = new int[4, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 }, { 5, 2, 6, 7 } };
+//Console.WriteLine(array.GetLength(0)); // количество строк
+//Console.WriteLine(array.GetLength(1)); // длина строки
+int summ = 0;
+
+
+int[] age;
+age = new int[array.GetLength(0)];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+    summ = array[i, j] + summ;
+    
+   }
+  
+    //Console.WriteLine($"цикл : {i}");
+    Console.WriteLine($"сумма строки {i}: {summ}");
+    age[i] = summ;
+    summ = 0; 
+}
+/* for (int k = 0; k < array.GetLength(0); k++)
+{
+    //Console.WriteLine($"найдем сумму массива age : {k}");
+    summ = age[k] + summ;
+    //Console.WriteLine($"цикл : {k}");
+    
+}
+Console.WriteLine($"Общая сумма: {summ}"); */
+
+
+ int min = age.Min ();
+ Console.WriteLine ($"Min: {min}");
+
+
+for (int i = 0; age[i] == age.Min (); i++)
+{
+    Console.WriteLine($"минимальная строка: {i+1}"); // как в примере без учета нулевой строки
+}
+
