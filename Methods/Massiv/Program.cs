5dea503086b7c785e9b722ea671c6703ebc78f9a
Alex <Lesnik290199@gmail.com>
2022-09-05 01:04:05 +0500

    решил задачу 56 семинара 8

--- Methods/Massiv/Program.cs
+++ Methods/Massiv/Program.cs
@@ -1,4 +1,91 @@
-﻿            // Определяем длинну массива
+﻿//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить 
+//строку с наименьшей суммой элементов.
+
+/* Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7 
+*/
+
+int[,] array = new int[4, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 }, { 5, 2, 6, 7 } };
+//Console.WriteLine(array.GetLength(0)); // количество строк
+//Console.WriteLine(array.GetLength(1)); // длина строки
+int summ = 0;
+
+
+int[] age;
+age = new int[array.GetLength(0)];
+//array[0, 1]; 
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+    summ = array[i, j] + summ;
+    
+   }
+  
+    //Console.WriteLine($"цикл : {i}");
+    Console.WriteLine($"сумма строки {i}: {summ}");
+    age[i] = summ;
+    summ = 0; 
+}
+for (int k = 0; k < array.GetLength(0); k++)
+{
+    //Console.WriteLine($"найдем сумму массива age : {k}");
+    summ = age[k] + summ;
+    //Console.WriteLine($"цикл : {k}");
+    
+}
+Console.WriteLine($"Общая сумма: {summ}");
+
+
+ int min = age.Min ();
+ Console.WriteLine ($"Min: {min}");
+
+
+for (int i = 0; age[i] == age.Min (); i++)
+{
+    Console.WriteLine($"минимальная строка: {i}");
+}
+
+
+
+
+
+
+
+
+// Two-dimensional array.
+int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+// The same array with dimensions specified.
+int[,] array2Da = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+// A similar array with string elements.
+string[,] array2Db = new string[3, 2] { { "one", "two" }, { "three", "four" },
+                                        { "five", "six" } };         
+//Доступ к элементам массива            
+System.Console.WriteLine(array2D[1, 0]);            
+            
+// Получение общего количества 
+//элементов или длины заданного измерения.  
+
+var allLength = array3D.Length;
+var total = 1;
+for (int i = 0; i < array3D.Rank; i++)
+{
+    total *= array3D.GetLength(i);
+}
+System.Console.WriteLine("{0} equals {1}", allLength, total);
+            
+// Присвоем значение элементу массива
+array5[2, 1] = 25;            
+            
+            
+            
+            // Определяем длинну массива
 
 int[] numbers = { 1, 2, 3, 5 };
 Console.WriteLine(numbers.Length); 
