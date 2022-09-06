1676e5369843bb5f0da147f7efd87a0662214738
Alex <Lesnik290199@gmail.com>
2022-09-06 16:52:48 +0500

    Решил ДЗ семинара 9

---
+++ HomeWork01Quarter/HomeWork09/Ex66/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+
+/* M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+
+
+Console.Write("Введите число N: ");
+int N = 0;
+int M = 0;
+int sum = 0;
+N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"-> ");
+while (N < M+1)
+{
+    sum = sum + N;
+    N ++;
+   }
+Console.Write($"{(sum)}");
